a7faf55e0ebf864ce7d7297cf7964f0fe717c4fe
Georgi St. Lukanov <goshiii87@gmail.com>
2024-05-10 15:49:41 +0300

    more functions

--- caller.py
+++ caller.py
@@ -7,7 +7,7 @@ from django.db.models import QuerySet
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ORM_testing.settings")
 django.setup()
 
-from main_app.models import Car
+from main_app.models import Car, Task
 
 
 def apply_discount() -> None:
@@ -39,3 +39,42 @@ def delete_last_car() -> None:
 # delete_last_car()
 # print('Cars count after deleting the last one: ')
 # print(Car.objects.all().count())
+
+
+def show_undefined_task() -> str:
+    unfinished_tasks = Task.objects.filter(is_finished=False)
+
+    return '\n'.join(str(t) for t in unfinished_tasks)
+
+
+# print(show_undefined_task())
+
+
+def complete_odd_task() -> None:
+    tasks = Task.objects.all()
+
+    for task in tasks:
+        if task.id % 2 != 0:
+            task.is_finished = True
+        task.save()
+
+
+# complete_odd_task()
+
+
+def encode_and_replace(text: str, task_title: str) -> None:
+    decoded_text = ''.join(chr(ord(x)) for x in text)
+    Task.objects.filter(title=task_title.lower()).update(description=decoded_text)
+
+
+#     searching_tasks = Task.objects.filter(title=task_title.lower())
+#     decoded_text = ''.join(chr(ord(x)) for x in text)
+#
+#     for task in searching_tasks:
+#         task.description = decoded_text
+#         task.save()
+#
+# encode_and_replace('asd$f323%#$3f#f3', 'Task - 1')
+# encode_and_replace('111#$3f#f3', 'task 4')
+# encode_and_replace('111#$3f#f3', 'Task 5')
+encode_and_replace('XXXXXX', 'Task 6')
