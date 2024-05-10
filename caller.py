e63d422ae39941a76e600ab6cbcb06f2adf27d38
Georgi St. Lukanov <goshiii87@gmail.com>
2024-05-10 15:04:47 +0300

    .

--- caller.py
+++ caller.py
@@ -1,6 +1,7 @@
 import os
 
 import django
+from django.db.models import QuerySet
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ORM_testing.settings")
@@ -19,4 +20,22 @@ def apply_discount() -> None:
         car.save()
 
 
-apply_discount()
+# apply_discount()
+
+
+def get_recent_cars() -> QuerySet:
+    return Car.objects.filter(year__gte=2020).values('model', 'price_with_discount')
+
+
+# print(get_recent_cars())
+# print(get_recent_cars().count())
+
+def delete_last_car() -> None:
+    # print('Cars count before delete the last one: ')
+    # print(Car.objects.all().count())
+    Car.objects.last().delete()
+
+
+# delete_last_car()
+# print('Cars count after deleting the last one: ')
+# print(Car.objects.all().count())
